ef978b83a1e554946320e6adf292b9486f7b49c1
Phil Plückthun <phil@plckthn.me>
2016-02-26 13:09:56 +0100

    Optimise style name performance slightly

--- src/Element.js
+++ src/Element.js
@@ -13,13 +13,13 @@ function styleCamelCase (name) {
 
   // Detect if the style property is already camelCased
   // To not convert Webkit*, Moz* and O* to lowercase
-  if (camel.slice(1) === name.slice(1)) {
-    return name
+  if (camel.charAt(0).toUpperCase() === name.charAt(0)) {
+    return name.charAt(0) + camel.slice(1)
   }
 
-  if (name[0] === '-') {
+  if (name.charAt(0) === '-') {
     return camel.indexOf('ms') === 0 ? camel
-      : camel[0].toUpperCase() + camel.slice(1)
+      : camel.charAt(0).toUpperCase() + camel.slice(1)
   } else {
     return camel
   }
