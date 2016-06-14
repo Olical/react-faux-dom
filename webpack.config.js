73b26412d6f9fea3912635a4efd0f9a15534b3a5
Kennet Postigo <kennetfpostigo@gmail.com>
2016-06-14 18:02:49 -0400

    added UMD build to project

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+var webpack = require('webpack');
+var env = process.env.NODE_ENV;
+
+var config = {
+
+  output: {
+    library: 'react-faux-dom',
+    libraryTarget: 'umd'
+  },
+
+  externals: [
+    {
+      react: {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react'
+      }
+    }
+  ],
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      }
+    ]
+  },
+
+  node: {
+    Buffer: false
+  },
+
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
+  ]
+
+}
+
+if (env === 'production') {
+  config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        screw_ie8: true,
+        warnings: false
+      }
+    })
+  )
+}
+
+module.exports = config;
